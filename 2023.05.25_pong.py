8d043e130d3453d31fadbcfe5800e8139e98f9bd
naaaaaoomiiiii <128704629+naaaaaoomiiiii@users.noreply.github.com>
2023-06-15 14:35:38 +0200

    startseite, punktestand angepasst

--- 2023.05.25_pong.py
+++ 2023.05.25_pong.py
@@ -2,6 +2,48 @@ import time
 import turtle
 import sys
 
+
+# Fenster erstellen
+window = turtle.Screen()
+window.title("Startseite")
+window.bgcolor("black")
+window.setup(width=800, height=600)
+
+# Turtle für den "Play"-Knopf erstellen
+play_button = turtle.Turtle()
+play_button.speed(0)
+play_button.color("white")
+play_button.penup()
+play_button.hideturtle()
+play_button.goto(0, 0)
+play_button.write("Play", align="center", font=("Courier", 24, "normal"))
+
+
+
+# Function
+
+def balken_a_up():
+    y = balken_a.ycor()
+    y += 20
+    balken_a.sety(y)
+
+def balken_a_down():
+    y = balken_a.ycor()
+    y -= 20
+    balken_a.sety(y)
+
+def balken_b_up():
+    y = balken_b.ycor()
+    y += 20
+    balken_b.sety(y)
+
+def balken_b_down():
+    y = balken_b.ycor()
+    y -= 20
+    balken_b.sety(y)
+
+
+
 width=800
 height=600
 wn = turtle.Screen()
@@ -10,9 +52,7 @@ wn.bgcolor("black")
 wn.setup(width, height)
 wn.tracer(0)
 
-# ergebnis
-ergebnis_a = 0
-ergebnis_b = 0
+
 
 # Balken A
 balken_a = turtle.Turtle()
@@ -51,27 +91,6 @@ pen.hideturtle()
 pen.goto(0, 260)
 pen.write("Player 1: 0 Player 2: 0", align="center", font=("Courier", 24, "normal"))
 
-# Function
-
-def balken_a_up():
-    y = balken_a.ycor()
-    y += 20
-    balken_a.sety(y)
-
-def balken_a_down():
-    y = balken_a.ycor()
-    y -= 20
-    balken_a.sety(y)
-
-def balken_b_up():
-    y = balken_b.ycor()
-    y += 20
-    balken_b.sety(y)
-
-def balken_b_down():
-    y = balken_b.ycor()
-    y -= 20
-    balken_b.sety(y)
 
 # Keybord binding
 wn.listen()
@@ -82,47 +101,74 @@ wn.onkeypress(balken_b_down, "Down")
 
 
 # main game loop
-while True:
-    time.sleep(0.001)
-    wn.update()
+def play_game(x, y):
+    # ergebnis
+    ergebnis_a = 0
+    ergebnis_b = 0
+    # Diese Funktion wird aufgerufen, wenn der Benutzer auf den "Play"-Knopf klickt
+    play_button.clear()
+    print("Spiel startet!")
+    while True:
+        time.sleep(0.001)
+        wn.update()
 
-    # move the ball
-    ball.setx(ball.xcor() + ball.dx)
-    ball.sety(ball.ycor() + ball.dy)
+        # move the ball
+        ball.setx(ball.xcor() + ball.dx)
+        ball.sety(ball.ycor() + ball.dy)
 
-    # border checking
-    if ball.ycor() > 290:
-        ball.sety(290)
-        ball.dy *= -1
+        # border checking
+        if ball.ycor() > 290:
+            ball.sety(290)
+            ball.dy *= -1
 
-    if ball.ycor() < -290:
-        ball.sety(-290)
-        ball.dy *= -1
+        if ball.ycor() < -290:
+            ball.sety(-290)
+            ball.dy *= -1
 
-    if ball.xcor() > 390:
-       ball.goto(0, 0)
-       ball.dx *= -1
-       ergebnis_a += 1
-       pen.clear()
-       pen.write("Player 1: {} Player 2: {}".format(ergebnis_a, ergebnis_b), align="center", font=("Courier", 24, "normal"))
-
-    if ball.xcor() < -390:
-       ball.goto(0, 0)
-       ball.dx *= -1
-       ergebnis_b += 1
-       pen.clear()
-       pen.write("Player 1: {} Player 2: {}".format(ergebnis_a, ergebnis_b), align="center", font=("Courier", 24, "normal"))
-
-    # Balken und Ball aufeinander prallen
-    if (ball.xcor() > 340 and ball.xcor() < 350) and (ball.ycor() < balken_b.ycor() + 40 and ball.ycor() > balken_b.ycor() - 40):
-        ball.setx(340)
-        ball.dx *= -1
-
-    if (ball.xcor() < -340 and ball.xcor() > -350) and (ball.ycor() < balken_a.ycor() + 40 and ball.ycor() > balken_a.ycor() - 40):
-        ball.setx(-340)
-        ball.dx *= -1
+        if ball.xcor() > 390:
+           ball.goto(0, 0)
+           ball.dx *= -1
+           ergebnis_a += 1
+           pen.clear()
+           pen.write("Player 1: {} Player 2: {}".format(ergebnis_a, ergebnis_b), align="center", font=("Courier", 24, "normal"))
+           if ergebnis_a > 8:
+               pen.clear()
+               pen.write("Winner: Player 1", align="center", font=("Courier", 24, "normal"))
+               time.sleep(5)
+               turtle.bye()
 
 
+
+        if ball.xcor() < -390:
+           ball.goto(0, 0)
+           ball.dx *= -1
+           ergebnis_b += 1
+           pen.clear()
+           pen.write("Player 1: {} Player 2: {}".format(ergebnis_a, ergebnis_b), align="center", font=("Courier", 24, "normal"))
+           if ergebnis_b > 8:
+               pen.clear()
+               pen.write("Winner: Player 2", align="center", font=("Courier", 24, "normal"))
+               time.sleep(5)
+               turtle.bye()
+
+
+
+
+
+        # Balken und Ball aufeinander prallen
+        if (ball.xcor() > 340 and ball.xcor() < 350) and (ball.ycor() < balken_b.ycor() + 50 and ball.ycor() > balken_b.ycor() - 50):
+            ball.setx(340)
+            ball.dx *= -1
+
+        if (ball.xcor() < -340 and ball.xcor() > -350) and (ball.ycor() < balken_a.ycor() + 40 and ball.ycor() > balken_a.ycor() - 40):
+            ball.setx(-340)
+            ball.dx *= -1
+
+# Den "Play"-Knopf anklickbar machen
+turtle.onscreenclick(play_game)
+
+# Hauptprogrammschleife ausführen
+turtle.mainloop()
 
 
 
